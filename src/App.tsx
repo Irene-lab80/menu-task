d2326abcadd69e8b0f056c9826e5dbbd2c576135
Irina <ivzhyurkevich@gmail.com>
2024-10-28 20:26:53 +0300

    feat: add markup for desktop

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 import { BellIcon, LocationIcon, LogoIcon } from "./shared";
-import style from "./App.module.css";
+import { useState } from "react";
+
+import style from "./App.module.scss";
+
+const menuData = [
+  { id: 1, title: "Преимущества Tele2" },
+  { id: 2, title: "Тарифы" },
+  { id: 3, title: "Акции и спецпредложения" },
+  { id: 4, title: "Промотариф Tele2" },
+  { id: 5, title: "Технология eSIM" },
+  { id: 6, title: "Подключение нового абонента" },
+];
 
 function App() {
+  const [active, setActive] = useState(menuData[0].id);
+
   return (
     <div>
       <header className={style.header}>
@@ -16,6 +29,20 @@ function App() {
           </div>
         </div>
       </header>
+
+      <nav className={style.nav}>
+        <ul>
+          {menuData.map((menuItem) => (
+            <li
+              className={active === menuItem.id ? style.itemActive : style.item}
+              key={menuItem.id}
+              onClick={() => setActive(menuItem.id)}
+            >
+              {menuItem.title}
+            </li>
+          ))}
+        </ul>
+      </nav>
     </div>
   );
 }
